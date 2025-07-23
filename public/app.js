f049c95290073899f7b69434c4f9ebb910d8e314
Reshard Manu <86658711+nyanyiwast@users.noreply.github.com>
2025-07-23 17:20:47 +0200

    generics basic usage

--- public/app.js
+++ public/app.js
@@ -1,26 +1,7 @@
-import { Invoice } from "./classes/Invoice.js";
-// classes
-// a blueprint of an object
-const invoiceOne = new Invoice('mario', 'work on the website', 4500);
-const invoiceTwo = new Invoice('luigi', 'work on the website', 5600);
-console.log(invoiceOne, invoiceTwo);
-let invoices = [];
-// type casting with keyword "as" to tell TS what type of element it is
-const form = document.querySelector('.new-item-form');
-console.log(form.children);
-invoices.push(invoiceOne);
-invoices.push(invoiceTwo);
-invoices.forEach(invoice => {
-    console.log(invoice.client, invoice.amount, invoice.format());
-});
-// inputs
-const type = document.querySelector('#type');
-const tofrom = document.querySelector('#tofrom');
-const details = document.querySelector('#details');
-const amount = document.querySelector('#amount');
-// submit event
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber //of type number
-    );
-});
+"use strict";
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOne = addUID({ name: 'yol', age: 40 });
+console.log(docOne);
--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,7 @@
-import {Invoice} from "./classes/Invoice.js";
+const addUID = <T extends object>(obj: T) => {
+    let uid = Math.floor(Math.random() *100);
+    return {...obj, uid}
+}
 
-// classes
-// a blueprint of an object
-
-const invoiceOne = new Invoice('mario', 'work on the website', 4500)
-const invoiceTwo = new Invoice('luigi', 'work on the website', 5600)
-
-console.log(invoiceOne, invoiceTwo)
-
-let invoices: Invoice[] = []
-
-// type casting with keyword "as" to tell TS what type of element it is
-const form = document.querySelector('.new-item-form') as HTMLFormElement;
-console.log(form.children);
-invoices.push(invoiceOne)
-invoices.push(invoiceTwo)
-
-invoices.forEach(invoice => {
-    console.log(invoice.client, invoice.amount, invoice.format())
-})
-
-// inputs
-const type = document.querySelector('#type') as HTMLSelectElement;
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
-
-// submit event
-form.addEventListener('submit', (e: Event) => {
-    e.preventDefault();
-    console.log(
-        type.value, 
-        tofrom.value, 
-        details.value,
-        amount.valueAsNumber //of type number
-    );
-})
+let docOne = addUID({name: 'yol', age: 40})
+console.log(docOne)
