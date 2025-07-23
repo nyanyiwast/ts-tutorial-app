76dec95615fe28a3bfa1b38a4b496d525e795f0d
Reshard Manu <86658711+nyanyiwast@users.noreply.github.com>
2025-07-23 15:36:34 +0200

    define re-usable types

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,32 +1,13 @@
 "use strict";
-let greet = () => {
-    console.log("Hello there!");
+const logDetails = (uid, item) => {
+    console.log(`${item} has uid of ${uid}`);
 };
-// greet = "hello" 
-let greetings = () => {
-    console.log("Hello strict there!");
+const greet = (user) => {
+    console.log(`${user.name} says hello`);
 };
-// greetings = "hello strict" // Error: Type 'string' is not assignable to type 'Function'.
-greetings();
-//Optional parameters in TypeScript
-const add = (a, b, c) => {
-    console.log(a + b);
-    console.log(c);
+const logDetailsWithAlias = (uid, item) => {
+    console.log(`${item} has uid of ${uid}`);
 };
-console.log(add(5, 10)); // Output: 15
-// Optional parameters can be used with default values where ? is not set
-// implicitly declare the function return type
-const addWithDefault = (a, b, c = 10) => {
-    console.log(a + b + c);
-    console.log(c);
-    return a + b + c;
+const greetWithAlias = (user) => {
+    console.log(`${user.name} says hello`);
 };
-addWithDefault(5, 10); // Output: 15
-// addWithDefault(5, 10, "20"); // Output: 15, "20"
-addWithDefault(5, 10, 2);
-const examPassRate = "60";
-const assessmentPassRate = "40";
-const obtainedExamMark = 80.4;
-const obtainedAssessmentMark = 72.5;
-let finalMark = ((obtainedExamMark / 100) * parseInt(examPassRate) + (obtainedAssessmentMark / 100) * parseInt(assessmentPassRate));
-console.log(`Final Mark: ${finalMark}%`); // Output: Final Mark: 100
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,39 +1,19 @@
-let greet = () => {
-    console.log("Hello there!")
+const logDetails = (uid: string | number, item: string) => {
+    console.log(`${item} has uid of ${uid}`);
 }
 
-// greet = "hello" 
-
-let greetings: Function = () => {
-    console.log("Hello strict there!")
+const greet = (user: {name: string, uid: string | number}) => {
+    console.log(`${user.name} says hello`);
 }
 
-// greetings = "hello strict" // Error: Type 'string' is not assignable to type 'Function'.
-greetings();
+// define our own type aliases
+type StringOrNum = string | number;
+type ObjectWithNum = {name: string, uid: StringOrNum};
 
-//Optional parameters in TypeScript
-const add = (a: number, b: number, c?: number|string) => {
-    console.log(a + b);
-    console.log(c);
-}
-console.log(add(5, 10)); // Output: 15
-
-// Optional parameters can be used with default values where ? is not set
-// implicitly declare the function return type
-const addWithDefault = (a: number, b: number, c: number = 10): number => {
-    console.log(a + b + c);
-    console.log(c);
-    return a + b + c;
+const logDetailsWithAlias = (uid: StringOrNum, item: string) => {
+    console.log(`${item} has uid of ${uid}`);
 }
 
-addWithDefault(5, 10); // Output: 15
-// addWithDefault(5, 10, "20"); // Output: 15, "20"
-addWithDefault(5, 10, 2);
-
-const examPassRate:string  = "60";
-const assessmentPassRate: string = "40"
-const obtainedExamMark: number = 80.4;
-const obtainedAssessmentMark: number = 72.5;
-
-let finalMark: number = ((obtainedExamMark / 100) * parseInt(examPassRate) + (obtainedAssessmentMark / 100) * parseInt(assessmentPassRate));
-console.log(`Final Mark: ${finalMark}%`); // Output: Final Mark: 100
+const greetWithAlias = (user: ObjectWithNum) => {
+    console.log(`${user.name} says hello`);
+}
